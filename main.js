3d09bb3d296cb3eea50c4d9fd86815d194acbafc
Antwand-T-Howard <antwand.howard@gmail.com>
2021-10-27 20:14:48 -0400

    Sprites, Bug Fixes, Sharper View

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ class Scene1 extends Phaser.Scene {
   bg = null;
   objGrid = null;
   ghost = null;
+  obj = {};
   
 constructor() { super(); }
 
@@ -26,10 +27,13 @@ preload()
 create()
 {
   var gridSize = this.gridSize;
+  var o;
+  
   // the camera
   var camera = this.cameras.main;
   camera.setBounds(0,0,this.width,this.height);
   camera.setZoom(2);
+  camera.roundPixels = true;
   // the background
   this.bg = this.add.image(0,0,'standardBg').setOrigin(0);
   var bg = this.bg;
@@ -43,6 +47,9 @@ create()
     camera.scrollX -= dragX;
     camera.scrollY -= dragY;
   });
+   
+  // the selected block highlight
+  this.obj["selectedBlockHighlight"] = this.add.rectangle(-999,-999,gridSize-1,gridSize-1,0xffffff,0.5);
   
   // the resource icons on the board
   var resources = ["food","water","wood"];
@@ -54,21 +61,20 @@ create()
   var icons = {};
   var thi = this;
   resources.forEach(function (s){
-    var o = thi.add.image(10,10,s);
+    var o = thi.add.image(-999,-999,s);
     //o = thi.add.circle(0,0,3,resourceColors[s]);
-    o.displayWidth = 8;
-    o.displayHeight = 8;
+    //o.displayWidth = 11;
+    //o.displayHeight = 11;
     icons[s] = o;
   });
   
-  // block selection
+  // block selection - works
   var _capture = {x:0,y:0};
   this.input.on('pointerdown', function(pointer){
     pointer.x /= camera.zoomX;
     pointer.x += (config.width/4);
     pointer.y /= camera.zoomY;
     pointer.y += (config.height/4);
-    
     pointer.x += camera.scrollX;
     pointer.y += camera.scrollY;
     _capture.x = pointer.x;
@@ -79,7 +85,6 @@ create()
     pointer.x += (config.width/4);
     pointer.y /= camera.zoomY;
     pointer.y += (config.height/4);
-    
     pointer.x += camera.scrollX;
     pointer.y += camera.scrollY;
     if (_capture.x == pointer.x && _capture.y == pointer.y) {
@@ -87,10 +92,12 @@ create()
       var y = Math.floor(pointer.y/gridSize);
       var jj = 0;
       resources.forEach(function(s){
-        if (getResources(x,y)[s] > 0) {
-          icons[s].x = x * gridSize + 2 + 7*(jj % 3);
-          icons[s].y = y * gridSize + 2 + 7*Math.floor(jj / 3);
-        
+        var g = getResources(x,y);
+        if (g[s] > 0) {
+          var yoffset = 7 - 4.5*(Math.floor((g.count-1)/2));
+          var xoffset = g.count == 1? 5 : 0;
+          icons[s].x = xoffset + x * gridSize + 5 + 11*(jj % 2);
+          icons[s].y = yoffset + y * gridSize + 3 + 11*Math.floor(jj / 2);
           jj++;
         }
         else {
@@ -99,6 +106,10 @@ create()
         }
       });
       camera.pan(pointer.x,pointer.y,200);
+      var o = thi.obj['selectedBlockHighlight'];
+      o.x = (x+.5) * gridSize - .5;
+      o.y = (y+.5) * gridSize - .5;
+      o.clock = 0;
     }
   });
   
@@ -110,15 +121,22 @@ create()
   var seeds = {};
   resources.forEach(function(s){
     maps[s] = new Array(sizeX);
-    for (var x=0; x<sizeX; x++)
+    for (var x=0; x<sizeX; x++) {
       maps[s][x] = new Array(sizeY);
+      for (var y=0; y<sizeY; y++) {
+        maps[s][x][y] = 0;
+      }
+    }
     seeds[s] = new SimplexNoise();
   });
   
   var getResources = function(_xx,_yy) {
     var o = {};
+    o.count = 0;
     resources.forEach(function(s){
-      o[s] = maps[s][_xx][_yy];
+      var m = maps[s][_xx][_yy];
+      o[s] = m;
+      if (m > 0) o.count++;
     });
     return o;
   }
@@ -126,31 +144,28 @@ create()
   for (var x=0;x<sizeX;x++) {
     for (var y = 0; y < sizeY; y++) {
       m = seeds.food.noise2D(x,y);
-      if (m > .3) {
+      if (m > .3)
         maps.food[x][y] = 1;
-        }
-      else
-        maps.food[x][y] = 0;
       
       m = seeds.water.noise2D(x,y);
-      if (m > .2) {
+      if (m > .2)
         maps.water[x][y] = 1;
-      }
-      else
-        maps.water[x][y] = 0;
       
       m = seeds.wood.noise2D(x, y);
-      if (m > .4) {
+      if (m > .4)
         maps.wood[x][y] = 1;
-      }
-      else
-        maps.wood[x][y] = 0;
     }
   }
   
-  this.ghost = this.physics.add.image(15,-25,'ghost');
-  this.ghost.mainX = 10;
-  this.ghost.mainY = -25;
+  // the ghost
+  this.obj['ghost'] = this.physics.add.image(15,-25,'ghost');
+  o = this.obj.ghost;
+  if (o) {
+    o.mainX = 20;
+    o.mainY = -35;
+  }
+  
+  // the HUD
   
 }
 
@@ -162,13 +177,20 @@ update(time, delta)
   camera.maxY = this.height-config.height;
   function clamp(num, min, max) { return num <= min ? min : num >= max ? max : num }
   
-  bg.x = clamp(camera.scrollX,0,camera.maxX) / 4;
-  bg.y = clamp(camera.scrollY,0,camera.maxY) / 4;
+  bg.x = clamp(camera.scrollX,0,camera.maxX) / 2;
+  bg.y = clamp(camera.scrollY,0,camera.maxY) / 2;
   
-  var ghost = this.ghost;
-  if (ghost) {
-    ghost.x = camera.scrollX + config.width/2 +ghost.mainX + 4*Math.cos(3*time/1500);
-    ghost.y = camera.scrollY + config.height/2 + ghost.mainY + 4*Math.sin(5*time/1500);
+  var o;
+  o = this.obj.ghost;
+  if (o) {
+    o.x = camera.scrollX + config.width/2 +o.mainX + 4*Math.cos(3*time/1500);
+    o.y = camera.scrollY + config.height/2 + o.mainY + 4*Math.sin(5*time/1500);
+  }
+  
+  o = this.obj.selectedBlockHighlight;
+  if (o) {
+    o.clock += delta * .005;
+    o.fillAlpha = Math.cos(o.clock) *.5+.5;
   }
 }
 }
@@ -180,7 +202,8 @@ var config = {
   physics: {
     default: 'arcade'
     },
-  scene: [ Scene1 ]
+  scene: [ Scene1 ],
+  antialias: false,
 };
 
 var game = new Phaser.Game(config);
